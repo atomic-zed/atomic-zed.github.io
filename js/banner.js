d23a4db81f3960f597eecc0d62b7cca47298c064
Zhao Ma <zma@atomic.vc>
2021-05-03 11:50:31 -0400

    different attempt

--- js/banner.js
+++
@@ -1,8 +0,0 @@
-if(localStorage.getItem('cookieSeen') != 'shown'){
-    document.querySelectorAll(".cookie-banner").delay(2000).fadeIn();
-    localStorage.setItem('cookieSeen','shown')
-}
-
-document.querySelectorAll('.close').click(function(e) {
-    document.querySelectorAll('.cookie-banner').fadeOut(); 
-});
